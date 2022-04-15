2e2f6cbf460a0d5df7e740147542de5a796e9dd3
ChrisMD2 <charlie.mike.1991@gmail.com>
2022-04-15 17:04:57 -0400

    Added a test module for the api

---
+++ test/app_web/inventory_api_test.exs
@@ -0,0 +1,118 @@
+defmodule App.InventoryApiTest do
+  use App.DataCase
+
+  alias App.InventoryApi
+
+  test "new/2 creates a new unit" do
+    request = %HTTPoison.Request{
+      method: :post,
+      url: "http://localhost:4002/api/inventory",
+      options: [],
+      headers: [
+        {~s|Content-Type|, ~s|application/json|},
+      ],
+      params: [],
+      body: ~s|{
+          "uuid":"value",
+          "name":"value",
+          "price":"value",
+          "category":"value",
+          "location_name":"value",
+          "location_area":"value"
+        }|
+    }
+
+    {:ok, response} = HTTPoison.request(request)
+    assert response.status_code == 201
+  end
+
+
+  test "import/2 imports the sent file" do
+    request = %HTTPoison.Request{
+      method: :post,
+      url: "http://localhost:4002/api/inventory/import",
+      options: [],
+      headers: [
+        {~s|Content-Type|, ~s|multipart/form-data|},
+      ],
+      params: [],
+      body: {:multipart, [
+              {:file, ~s|support/test_data.csv|}
+          ]}
+    }
+
+    {:ok, response} = HTTPoison.request(request)
+    assert response.status_code == 200
+  end
+
+  defp get_setup do
+    request = %HTTPoison.Request{
+      method: :post,
+      url: "http://localhost:4002/api/inventory/import",
+      options: [],
+      headers: [
+        {~s|Content-Type|, ~s|multipart/form-data|},
+      ],
+      params: [],
+      body: {:multipart, [
+              {:file, ~s|support/test_data.csv|}
+          ]}
+    }
+
+    {:ok, response} = HTTPoison.request(request)
+  end
+  defp test_get_helper(params \\ %{}) do
+    params = Map.keys(params)
+    |> Enum.map(fn(key) ->
+      {~s|#{key}|, ~s|#{Map.fetch!(params, key)}|}
+    end)
+
+    request = %HTTPoison.Request{
+      method: :get,
+      url: "http://localhost:4002/api/inventory",
+      options: [],
+      headers: [],
+      params: params,
+      body: ""
+    }
+
+    {:ok, response} = HTTPoison.request(request)
+    response
+  end
+  test "get/2 gets all the records" do
+    response = test_get_helper
+    assert response.status_code == 200
+  end
+  defp value_fixer(value) do
+    value = case value do
+      "[]" -> []
+      _ -> value
+    end
+  end
+  defp map_fix(json_string) do
+    a_map = Jason.decode!(json_string)
+    Map.keys(a_map)
+    |> Enum.map(fn(key) ->
+      value = Map.fetch!(a_map, key)
+      {status, t_val} = Jason.decode(value)
+      if status == :ok do
+        {key, value_fixer(t_val) }
+      else
+        {key, value_fixer(value) }
+      end
+    end)
+    |> Map.new
+  end
+  test "get/2 gets the records based on provided params" do
+    get_setup
+    response = test_get_helper(%{"location_area"=>"value"})
+    assert response.status_code == 200
+    assert map_fix(response.body) == %{"message" => []}
+    response = test_get_helper(%{"location_area"=>"test_area_2"})
+    assert response.status_code == 200
+    # assert Map.fetch!(map_fix(response.body), "message") == true
+    response = test_get_helper(%{"location_area"=>"test_area_2","location_name"=>"Location 5"})
+    assert response.status_code == 200
+    # assert Jason.decode!(response.body) == true
+  end
+end
